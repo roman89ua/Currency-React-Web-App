83fe003039b456099c70aba9028fbb57483b2686
Roman Kostiuk <54412595+roman89ua@users.noreply.github.com>
2022-11-07 13:24:30 +0200

    public static fixes

--- Currency-React-Web-App/Clients/CurrencyClient.cs
+++ Currency-React-Web-App/Clients/CurrencyClient.cs
@@ -8,36 +8,34 @@ namespace Currency_React_Web_App.Clients
 {
     public class CurrencyClient : ICurrencyClient
     {
-        private readonly HttpClient _httpClient;
+        private static HttpClient? _httpClient;
 
-        private readonly FetchDataService _dbService;
+        private static FetchDataService? _dbService;
 
-        public CurrencyClient(HttpClient httpClient, MongoClientBase mongoClient)
+        public CurrencyClient(HttpClient? httpClient, MongoClientBase mongoClient)
         {
             _httpClient = httpClient;
             _dbService = new FetchDataService(mongoClient);
         }
-        
-        public async Task<List<CurrentDateCurrencyModel>> DataBaseRefresh()
+
+        public async Task DataBaseRefresh()
         {
-            HttpResponseMessage response = await _httpClient.GetAsync("NBUStatService/v1/statdirectory/exchangenew?json");
+            HttpResponseMessage response =
+                await _httpClient?.GetAsync("NBUStatService/v1/statdirectory/exchangenew?json")!;
             dynamic responseData = response.Content.ReadAsStringAsync().Result;
             var data = JsonConvert.DeserializeObject<List<CurrentDateCurrencyModel>>(responseData);
             List<CurrentDateCurrencyModel> dataFetchResult = data.ToList<CurrentDateCurrencyModel>();
             if (dataFetchResult.Count > 0)
             {
-                await _dbService.GetCurrentDateCurrencyCollection().DeleteManyAsync(item => item.Id >= 0);
+                await _dbService!.GetCurrentDateCurrencyCollection().DeleteManyAsync(item => item.Id >= 0);
                 await _dbService.GetCurrentDateCurrencyCollection().InsertManyAsync(dataFetchResult);
             }
-
-            return dataFetchResult;
-           
         }
     }
 
     public interface ICurrencyClient
     {
-        public Task<List<CurrentDateCurrencyModel>> DataBaseRefresh();
+        public Task DataBaseRefresh();
     }
 }
 
